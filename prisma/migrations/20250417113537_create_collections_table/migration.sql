8df8cd441077238e7be1db9f2d8774acbc26a3d0
souza <sarasouza.tds2.senai@gmail.com>
2025-04-17 09:05:40 -0300

    feat: create collections and cards tables

---
+++ prisma/migrations/20250417113537_create_collections_table/migration.sql
@@ -0,0 +1,39 @@
+-- CreateTable
+CREATE TABLE "animes" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "episodes" INTEGER NOT NULL,
+    "releaseYear" INTEGER NOT NULL,
+    "studio" TEXT NOT NULL,
+    "genres" TEXT NOT NULL,
+    "rating" REAL NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "personagens" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "age" INTEGER NOT NULL,
+    "power" BOOLEAN NOT NULL,
+    "anime" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "collections" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "releaseYear" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "collections_name_key" ON "collections"("name");
